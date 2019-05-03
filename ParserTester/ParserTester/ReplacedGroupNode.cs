f2e1af6696d1b8db8f2e7f036f45e129530bf76f
YogurtTheHorse <yegorf1@gmail.com>
2019-05-03 16:24:59 +0300

    Add replacing group and terminal nodes

---
+++ ParserTester/ParserTester/ReplacedGroupNode.cs
@@ -0,0 +1,36 @@
+﻿using ParserImpl;
+using ParserImpl.Grammar;
+using ParserImpl.Impl;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ParserTester
+{
+    public class ReplacedGroupNode : IParsingTreeGroup
+    {
+        private IParsingTreeGroup _original;
+
+        public ReplacedGroupNode(IParsingTreeGroup original, IEnumerable<IParsingTreeNode> childs)
+        {
+            _original = original;
+            Childs = childs;
+        }
+
+        public IEnumerable<IParsingTreeNode> Childs { get; }
+
+        public int ChildsCount => Childs.Count();
+
+        public ParserNode GrammarNode => _original.GrammarNode;
+
+        public Location Location => _original.Location;
+
+        public Rule Rule => _original.Rule;
+
+        public RuleExpression Expression => _original.Expression;
+
+        public void Visit(IParsingTreeNodeVisitor visitor)
+        {
+            visitor.VisitGroup(this);
+        }
+    }
+}
---
+++ ParserTester/ParserTester/ReplacedTerminal.cs
@@ -0,0 +1,39 @@
+﻿using ParserImpl;
+using ParserImpl.Grammar;
+using ParserImpl.Impl;
+using System;
+
+namespace ParserTester
+{
+    public class ReplacedNode : IParsingTreeTerminal
+    {
+        private IParsingTreeTerminal _original;
+
+        public ReplacedNode(IParsingTreeTerminal originalTerminal, string content, Location from, Location to)
+        {
+            _original = originalTerminal;
+            From = from;
+            To = to;
+            Content = content;
+        }
+
+        public ParserNode GrammarNode => _original.GrammarNode;
+
+        public Location Location => From;
+
+        public Rule Rule => _original.Rule;
+
+        public RuleExpression Expression => _original.Expression;
+
+        public Location From { get; }
+
+        public Location To { get; }
+
+        public string Content { get; }
+
+        public void Visit(IParsingTreeNodeVisitor visitor)
+        {
+            visitor.VisitTerminal(this);
+        }
+    }
+}
